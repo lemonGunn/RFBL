35a1f8a0b02e78b2883d15510aeee25ace5d88cb
Chao geng <335375158@qq.com>
2017-07-24 15:01:37 +0800

    20170224

---
+++ Holo-Winnower-5.6/Assets/ProjectAsset/ScenesTest/Scripts/TapTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class TapTest : MonoBehaviour, IInputClickHandler
+{
+    public GameObject cube;
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        cube.GetComponent<Renderer>().material.color = new Color(UnityEngine.Random.Range(0, 0.5F), UnityEngine.Random.Range(0, 0.5F), UnityEngine.Random.Range(0, 0.5F));
+    }
+}
